3f2943fbf60ca0645124047541aa6c3eca9817ab
Aladin <dala98058@gmail.com>
2023-10-07 11:58:30 -0600

    Enable admin for user profile models

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -13,8 +13,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Enter your email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
-
+        user = self.model(email=email,name=name)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -23,7 +22,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
-        user= self.create_user(email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -45,7 +44,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQURED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Retrieve full name of user"""
